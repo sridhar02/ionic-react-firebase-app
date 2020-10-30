7a107b9700bc4c93bb133c1183448bda81b489ab
sridhar02 <kattasridhar02@gmail.com>
2020-10-30 13:09:02 +0530

    simple ionic app with list items and slide options

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,73 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonHeader,
+  IonList,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonItem,
+  IonText,
+  IonItemSliding,
+  IonItemOptions,
+  IonItemOption,
+  IonAvatar,
+  IonLabel,
+} from "@ionic/react";
+import React from "react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Home.css";
+
+const arr = [
+  { name: "sridhar", description: "Hello world!" },
+  {
+    name: "katta",
+    description: "adasdasdad",
+  },
+  {
+    name: "kta",
+    description: "adasdasdad",
+  },
+  {
+    name: "kata",
+    description: "adasdasdad",
+  },
+];
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Hello world</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent>
+        <IonTitle class="ion-padding">Ionic is interesting!!!!</IonTitle>
+        <IonList>
+          {arr.map((elem, i) => (
+            <IonItemSliding key={elem.name}>
+              <IonItem>
+                <IonAvatar>
+                  <img src={`//unsplash.it//${400 + i}`} alt="random logo" />
+                </IonAvatar>
+
+                <IonLabel className="ion-padding">
+                  <h2>{elem.name}</h2>
+                  <h3>{elem.description}</h3>
+                  <p>Some helper text!!!</p>
+                </IonLabel>
+              </IonItem>
+
+              <IonItemOptions side="end">
+                <IonItemOption
+                  onClick={() => alert("are you sure want to delete")}
+                >
+                  Delete
+                </IonItemOption>
+              </IonItemOptions>
+            </IonItemSliding>
+          ))}
+        </IonList>
       </IonContent>
     </IonPage>
   );
